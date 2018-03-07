297c0c3e50ebe07d54060e2854ad3144341bab8a
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-07 16:42:02 -0500

    Initial commit

---
+++ lib/Renard/Incunabula/Cairo.pm
@@ -0,0 +1,34 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Incunabula::Cairo;
+# ABSTRACT: Helper library for the Cairo graphics library
+
+use Cairo;
+
+package Cairo::Matrix {
+	use Renard::Incunabula::Common::Setup;
+
+	method _unpack_struct() {
+		( unpack 'd6', unpack 'P48', pack 'J', $$self )
+	}
+
+=method Cairo::Matrix::to_HashRef
+
+Returns a C<HashRef> with the keys C<xx yx xy yy x0 y0> that represent the
+matrix components.
+
+=cut
+	method to_HashRef() {
+		my @fields = qw(xx yx xy yy x0 y0);
+		my @data = $self->_unpack_struct;
+
+		+{ map { $fields[$_] => $data[$_] } 0..@fields-1 };
+	}
+
+}
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Cairo/>
+
+=cut
---
+++ t/Cairo/Matrix.t
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use Test::Most tests => 1;
+use Renard::Incunabula::Common::Setup;
+use Renard::Incunabula::Cairo;
+
+subtest "to_HashRef" => sub {
+	subtest "scale" => sub {
+		my $matrix = Cairo::Matrix->init_scale(2, 3);
+		is_deeply $matrix->to_HashRef,
+			{ xx => 2, yy => 3, yx => 0, xy => 0, x0 => 0, y0 => 0, }, 'got the right values';
+	};
+
+	subtest "translate" => sub {
+		my $matrix = Cairo::Matrix->init_translate(4, 8);
+		is_deeply $matrix->to_HashRef,
+			{ xx => 1, yy => 1, yx => 0, xy => 0, x0 => 4, y0 => 8, }, 'got the right values';
+	};
+};
+
+done_testing;
